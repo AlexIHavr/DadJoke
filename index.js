89e1aee3bb6eea04344b5e8ab75e37d1cee4ae1e
AlexIHavr <dchavronin@bk.ru>
2022-01-05 16:28:17 +0300

    first commit

--- index.js
+++ index.js
@@ -1,99 +1,74 @@
 import https from 'https';
 import fs from 'fs';
 
-class DadJokeAPI {
-  constructor() {
-    this.leaderboard = process.argv[2] === '--leaderboard';
-    this.setOptions();
-    this.setAllJokes();
-  }
+const leaderboard = process.argv[2] === '--leaderboard';
+const searchTerm = process.argv[2] === '--searchTerm' ? process.argv[3] : undefined;
 
-  async getDadJoke() {
-    if (this.leaderboard) {
-      return this.getLeaderboardJoke();
-    }
-
-    return await this.getSearchTermJoke(this.options);
-  }
-
-  setOptions() {
-    if (this.leaderboard) return;
-
-    const searchTerm = process.argv[2] === '--searchTerm' ? process.argv[3] : undefined;
-
-    if (searchTerm === undefined) {
-      throw new Error('Enter, please, searchTerm argument.');
-    }
-
-    this.options = {
-      host: 'icanhazdadjoke.com',
-      path: `/search?term=${searchTerm}`,
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-      },
-    };
-  }
-
-  setAllJokes() {
-    try {
-      this.allJokes = JSON.parse(fs.readFileSync('./jokes.json'));
-    } catch (e) {
-      this.allJokes = [];
-    }
-  }
-
-  getLeaderboardJoke() {
-    if (!this.allJokes.length) return 'jokes.json has no jokes.';
-    return this.allJokes.sort((a, b) => b.count - a.count)[0].joke;
-  }
-
-  async getSearchTermJoke(options) {
-    return new Promise((resolve, reject) => {
-      https
-        .get(options, (res) => {
-          this.data = '';
-
-          res.on('data', (chunk) => {
-            this.data += chunk;
-          });
-
-          res.on('end', () => {
-            resolve(this.getRandomJoke());
-          });
-        })
-        .on('error', (e) => {
-          throw new Error(e.message);
-        });
-    });
-  }
-
-  getRandomJoke() {
-    const jokes = JSON.parse(this.data).results;
-
-    if (!jokes.length) return 'No jokes were found for that search term';
-
-    const randomJoke = jokes[this.getRandomNumber(0, jokes.length - 1)];
-
-    const indexExistJoke = this.allJokes.findIndex(({ id }) => id === randomJoke.id);
-    if (indexExistJoke !== -1) {
-      this.allJokes[indexExistJoke].count += 1;
-    } else {
-      this.allJokes.push({ ...randomJoke, count: 1 });
-    }
-
-    fs.writeFileSync('./jokes.json', JSON.stringify(this.allJokes));
-
-    return randomJoke.joke;
-  }
-
-  getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+let allJokes;
+try {
+  allJokes = JSON.parse(fs.readFileSync('./jokes.json'));
+} catch (e) {
+  allJokes = [];
 }
 
-const dadJokeAPI = new DadJokeAPI();
+if (leaderboard) {
+  console.log(getLeaderboardJoke());
+  process.exit();
+}
 
-(async function () {
-  console.log(await dadJokeAPI.getDadJoke());
-})();
+if (searchTerm === undefined) {
+  throw new Error('Enter, please, searchTerm argument.');
+}
+
+const options = {
+  host: 'icanhazdadjoke.com',
+  path: `/search?term=${searchTerm}`,
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+  },
+};
+
+https
+  .get(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      console.log(getRandomJoke(data));
+    });
+  })
+  .on('error', (e) => {
+    throw new Error(e.message);
+  });
+
+function getLeaderboardJoke() {
+  if (!allJokes.length) return 'jokes.json has no jokes.';
+  return allJokes.sort((a, b) => b.count - a.count)[0].joke;
+}
+
+function getRandomJoke(data) {
+  const jokes = JSON.parse(data).results;
+
+  if (!jokes.length) return 'No jokes were found for that search term';
+
+  const randomJoke = jokes[getRandomNumber(0, jokes.length - 1)];
+
+  const indexExistJoke = allJokes.findIndex(({ id }) => id === randomJoke.id);
+  if (indexExistJoke !== -1) {
+    allJokes[indexExistJoke].count += 1;
+  } else {
+    allJokes.push({ ...randomJoke, count: 1 });
+  }
+
+  fs.writeFileSync('./jokes.json', JSON.stringify(allJokes));
+
+  return randomJoke.joke;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
