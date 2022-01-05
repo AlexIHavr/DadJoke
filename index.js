7d817c67cf2af82006391fe5fb89bd1b99bd3792
AlexIHavr <dchavronin@bk.ru>
2022-01-05 13:23:49 +0300

    refactoring all

--- index.js
+++ index.js
@@ -1,60 +1,81 @@
 import https from 'https';
 import fs from 'fs';
 
-const searchTerm = process.argv[2];
-const leaderboard = process.argv[3];
+const searchTerm = process.argv[2] === '--searchTearm' ? process.argv[3] : null;
+const leaderboard = process.argv[2] === '--leaderboard';
 
-const options = {
+class DadJokeAPI {
+  constructor(options) {
+    this.options = options;
+    this.allJokes = JSON.parse(fs.readFileSync('./jokes.json'));
+  }
+
+  async getDadJoke() {
+    if (leaderboard) {
+      return this.getLeaderboardJoke();
+    }
+
+    return await this.getSearchTermJoke(this.options);
+  }
+
+  getLeaderboardJoke() {
+    if (!this.allJokes.length) return 'jokes.json has no jokes.';
+    return this.allJokes.sort((a, b) => a.count - b.count).slice(-1)[0].joke;
+  }
+
+  async getSearchTermJoke(options) {
+    return new Promise((resolve, reject) => {
+      https
+        .get(options, (res) => {
+          this.data = '';
+
+          res.on('data', (chunk) => {
+            this.data += chunk;
+          });
+
+          res.on('end', () => {
+            resolve(this.getRandomJoke());
+          });
+        })
+        .on('error', (e) => {
+          throw new Error(e.message);
+        });
+    });
+  }
+
+  getRandomJoke() {
+    const jokes = JSON.parse(this.data).results;
+
+    if (!jokes.length) return 'No jokes were found for that search term';
+
+    const randomJoke = jokes[this.getRandomNumber(0, jokes.length - 1)];
+
+    const indexExistJoke = this.allJokes.findIndex(({ id }) => id === randomJoke.id);
+    if (indexExistJoke !== -1) {
+      this.allJokes[indexExistJoke].count += 1;
+    } else {
+      this.allJokes.push({ ...randomJoke, count: 1 });
+    }
+
+    fs.writeFileSync('./jokes.json', JSON.stringify(this.allJokes));
+
+    return randomJoke.joke;
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
+const dadJokeAPI = new DadJokeAPI({
   host: 'icanhazdadjoke.com',
   path: `/search?term=${searchTerm}`,
   method: 'GET',
   headers: {
     Accept: 'application/json',
   },
-};
+});
 
-const getJokes = (data) => {
-  const jokes = JSON.parse(data).results;
-
-  if (!jokes.length) return console.log('No jokes were found for that search term');
-
-  const randomJoke = jokes[getRandomNumber(0, jokes.length - 1)];
-
-  const allJokes = JSON.parse(fs.readFileSync('./jokes.json'));
-
-  if (leaderboard) {
-    console.log(allJokes.sort((a, b) => a.count - b.count).slice(-1));
-    return;
-  }
-
-  const indexExistJoke = allJokes.findIndex(({ id }) => id === randomJoke.id);
-  if (indexExistJoke !== -1) {
-    allJokes[indexExistJoke].count += 1;
-  } else {
-    allJokes.push({ ...randomJoke, count: 1 });
-  }
-
-  fs.writeFileSync('./jokes.json', JSON.stringify(allJokes));
-
-  console.log(randomJoke);
-};
-
-https
-  .get(options, (res) => {
-    let body = '';
-
-    res.on('data', (data) => {
-      body += data;
-    });
-
-    res.on('end', () => {
-      getJokes(body);
-    });
-  })
-  .on('error', (e) => {
-    console.error(e);
-  });
-
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+(async function () {
+  console.log(await dadJokeAPI.getDadJoke());
+})();
